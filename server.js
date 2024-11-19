05408da597fd52c321c00da1eb4e21ab22060a6d
Rogério <rogeriodpaguilarbr@gmail.com>
2024-11-19 14:22:14 -0300

    aulaDois

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
  
 const app = express();
+app.use(express.json());
+
+
+
 
 app.listen(3000, () =>{console.log("Servidor express escutando");});
 
-app.get('/api', (req, resp) => {resp.status(200).send("Bem vindo à imersão")});
 
-
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Gato fazendo yoga", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150"},
+];
 
 const jsonLivro = 
 {
@@ -16,5 +23,13 @@ const jsonLivro =
   "genero": "Fantasia"
 };
 
+app.get('/api', (req, resp) => {resp.status(200).send("Bem vindo à imersão")});
+app.get('/api/livro', (req, resp) => {resp.status(200).json(jsonLivro)});
+app.get('/api/posts', (req, resp) => {resp.status(200).json(posts)});
 
-app.get('/api/livro', (req, resp) => {resp.status(200).send(jsonLivro)});
+app.get('/api/post/:id', (req, resp) => {
+    const postIndex = posts.findIndex((post)=>{return post.id == Number(req.params.id)});
+
+    resp.status(200).json(posts[postIndex])
+});
+
